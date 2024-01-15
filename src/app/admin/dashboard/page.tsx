0b35e64e01f0f2cd98f9f2b29f6907f5539bcf0b
royandugu <royandugu99009@gmail.com>
2024-01-15 20:55:10 +0545

    Removed session check

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,13 +1,13 @@
-import { getServerSession } from "next-auth";
-import { options } from "@/app/api/auth/[...nextauth]/options";
-import { redirect } from "next/navigation";
+// import { getServerSession } from "next-auth";
+// import { options } from "@/app/api/auth/[...nextauth]/options";
+// import { redirect } from "next/navigation";
 
 import DashboardDisplay from "../../../../components/adminComponents/secondaryComponents/dashboardDisplay";
 
 const Page=async ()=>{
-    const sessionData=await getServerSession(options);
+    //const sessionData=await getServerSession(options);
 
-    if(sessionData) return <DashboardDisplay/>
-    else return redirect('api/auth/signin?callbackUrl=/server');
+    return <DashboardDisplay/>
+    //else return redirect('api/auth/signin?callbackUrl=/server');
 }
 export default Page;
